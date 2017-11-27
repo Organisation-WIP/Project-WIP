f91e587c1d9595042416ea084f371dab2b377b08
NotPrepared <33688325+NotPrepared@users.noreply.github.com>
2017-11-27 13:51:50 +0100

    Update enemyship.cpp

--- models/enemyship.cpp
+++ models/enemyship.cpp
@@ -2,6 +2,34 @@
 
 EnemyShip::EnemyShip()
 {
+  QString patch(SRCDIR);
+  m_ShipMesh = new TriangleMesh(path+"/Object/enemyship.obj");
+  m_ShipDraw = new Drawable(m_ShipMesh);
+  m_ShipBone = new Bone("ship", this);
+  Node* TransNode = new Node(m_ShipBone);
+  TransNode->addChild(new Node(m_ShipDraw));
+  this->addChild(TransNode);
+}
 
+EnemyShip::~EnemyShip()
+{
+  delete m_ShipMesh;
+  delete m_ShipDraw;
+  delete m_ShipBone;
+}
+
+void EnemyShip::translate(float tx, float ty, float tz)
+{
+    m_ShipBone->translate(tx, ty, tz);
+}
+
+void EnemyShip::rotate(float angle, const QVector3D &axis)
+{
+    m_ShipBone->rotate(angle, axis);
+}
+
+void EnemyShip::rotate(float angle, float x, float y, float z)
+{
+    m_ShipBone->rotate(angle, x, y, z);
 }
 
