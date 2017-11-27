b0f1690566487653dfdcbd5049878d6545c7d3ea
NotPrepared <33688325+NotPrepared@users.noreply.github.com>
2017-11-27 13:42:43 +0100

    Update enemyship.h

--- models/enemyship.h
+++ models/enemyship.h
@@ -2,12 +2,25 @@
 #define ENEMYSHIP_H
 
 #include "node.h"
+#include "frameworkExtension/model.h"
+#include "drawable.h"
+#include "trianglemesh.h"
+#include "transformation.h"
 
 
 class EnemyShip : public Node
 {
-public:
-    EnemyShip();
+    Tower();
+    ~Tower();
+
+    void translate(float tx, float ty, float tz);
+    void rotate(float angle, const QVector3D& axis);
+    void rotate(float angle, float x, float y, float z);
+
+private:
+    TriangleMesh *m_ShipMesh;
+    Drawable *m_ShipDraw;
+    Bone *m_ShipBone;
 };
 
 #endif // ENEMYSHIP_H
